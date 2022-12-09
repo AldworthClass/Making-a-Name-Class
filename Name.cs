c822add255c89b82e1600ff90599629398cb2696
Steve Aldworth <s.aldworth@tvdsb.ca>
2022-12-09 13:20:33 -0500

    Add project files.

---
+++ Name.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Making_a_Name_Class
+{
+    public class Name
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _title;
+
+        public Name(string firstName, string lastName, string title)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _title = title; 
+        }
+
+        public Name(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _title = "";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Making_a_Name_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
