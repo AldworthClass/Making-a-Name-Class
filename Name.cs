a8c803829a0cef7e8e9006c346610e1fbb992916
Steve Aldworth <s.aldworth@tvdsb.ca>
2022-12-12 14:00:13 -0500

    finished

--- Name.cs
+++ Name.cs
@@ -8,11 +8,11 @@ namespace Making_a_Name_Class
 {
     public class Name
     {
+        private string _title;
         private string _firstName;
         private string _lastName;
-        private string _title;
 
-        public Name(string firstName, string lastName, string title)
+        public Name(string title, string firstName, string lastName)
         {
             _firstName = firstName;
             _lastName = lastName;
@@ -25,6 +25,32 @@ namespace Making_a_Name_Class
             _lastName = lastName;
             _title = "";
         }
+
+        public string FirstName 
+        {  
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public string Initials
+        {
+            get { return _firstName.Substring(0, 1) + _lastName.Substring(0, 1); }
+        }
+
+        public override string ToString() { 
+            return (_title + " " + _firstName + " " + _lastName).Trim(); 
+        }
+
     }
 }
 
--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace Making_a_Name_Class
+﻿using System.Runtime.InteropServices;
+
+namespace Making_a_Name_Class
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Name student;
+            Name beatle = new Name("Sir", "Paul", "McCartney");
+            Console.WriteLine(beatle.FirstName);
+            Console.WriteLine(beatle.LastName);
+            Console.WriteLine(beatle);  // The ToString() method is automatically called Here
+            Console.WriteLine(beatle.Initials);
+            beatle.FirstName = "James";
+            Console.WriteLine(beatle);
+            Console.WriteLine(beatle.Initials);
+            student = new Name("Jane", "Doe");
+            Console.WriteLine(student);
         }
     }
 }
